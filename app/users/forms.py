d451bcd22b8e8239e0e6291ac0658cc993cf5190
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-01-15 16:14:11 +0300

    Add a form

---
+++ app/users/forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileFiled, FileRequired
+from flask_wtf import StringField,PasswordField,SubmitFied
+
+
+class RegistrationForm(FlaskForm)
+    
